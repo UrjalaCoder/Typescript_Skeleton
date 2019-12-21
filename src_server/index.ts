72c5f54cbbf05774e3fe464852463abe4c12c671
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2019-12-22 01:33:54 +0200

    Revert "Updates"
    This reverts commit 30c08dd5e176f658961b02b8f67647adf0da9bd8.

--- src_server/index.ts
+++ src_server/index.ts
@@ -1,47 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-
-interface Message {
-  user: string;
-  message: string;
-  timestamp: number;
-  id: number;
-}
+import path from 'path';
 
 const app: express.Application = express();
 
-app.use(bodyParser.json());
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
-const messages: Message[] = []
-const oldIds: number[] = []
-
-app.get('/', (_req: express.Request, res: express.Response) => {
-  res.send(JSON.stringify({
-    messages,
-    messageCount: messages.length
-  }));
+app.get('/', (_req: express.Request, res: express.Response): void => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/', (req: express.Request, res: express.Response) => {
-
-  let newId = Math.floor(Math.random() * 30000)
-  while(oldIds.indexOf(newId) > -1) {
-    newId = Math.floor(Math.random() * 30000)
-  }
-  const newMessage: Message = {
-    ...req.body,
-    timestamp: Date.now(),
-    id: newId
-  }
-
-  oldIds.push(newId);
-  messages.push(newMessage);
-  res.send(JSON.stringify({
-    success: true
-  }));
-});
-
-
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log("Server started!");
 });
