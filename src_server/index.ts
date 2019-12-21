30c08dd5e176f658961b02b8f67647adf0da9bd8
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2019-12-22 01:32:11 +0200

    Updates

--- src_server/index.ts
+++ src_server/index.ts
@@ -1,14 +1,47 @@
 import express from 'express';
-import path from 'path';
+import bodyParser from 'body-parser';
+
+interface Message {
+  user: string;
+  message: string;
+  timestamp: number;
+  id: number;
+}
 
 const app: express.Application = express();
 
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
 
-app.get('/', (_req: express.Request, res: express.Response): void => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const messages: Message[] = []
+const oldIds: number[] = []
+
+app.get('/', (_req: express.Request, res: express.Response) => {
+  res.send(JSON.stringify({
+    messages,
+    messageCount: messages.length
+  }));
 });
 
-app.listen(3000, () => {
+app.post('/', (req: express.Request, res: express.Response) => {
+
+  let newId = Math.floor(Math.random() * 30000)
+  while(oldIds.indexOf(newId) > -1) {
+    newId = Math.floor(Math.random() * 30000)
+  }
+  const newMessage: Message = {
+    ...req.body,
+    timestamp: Date.now(),
+    id: newId
+  }
+
+  oldIds.push(newId);
+  messages.push(newMessage);
+  res.send(JSON.stringify({
+    success: true
+  }));
+});
+
+
+app.listen(3001, () => {
   console.log("Server started!");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,5 @@
 const path = require('path');
 
-const client = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
-
 const server = {
   entry: './src_server/index.ts',
   mode: 'development',
@@ -46,4 +25,4 @@ const server = {
   }
 };
 
-module.exports = [client, server];
+module.exports = [server];
